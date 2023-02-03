5be62aaa4984d341c1fe63a9beae935d2729c5e9
Fanzil95 <amirov_fanzil@mail.ru>
2023-02-03 22:06:30 +0300

    fix

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,16 @@
 
-//f-fromCoordinate, t-toCoordinate
-
-export function getListGoordinates (f,t, func)  {
-    fetch(`http://router.project-osrm.org/route/v1/driving/${f};${t}?alternatives=false&steps=false&geometries=geojson&overview=full&annotations=false`)
-        .then(response => response.json())
-        .then(poly => {
-        poly.routes.map(route=>func(route.geometry.coordinates))
-        })
+export async function getListGoordinates (f,t, func) {
+    try{
+        const requests = await fetch(`http://router.project-osrm.org/route/v1/driving/${f};${t}?alternatives=false&steps=false&geometries=geojson&overview=full&annotations=false`)
+        const polylines = await requests.json()
+        polylines.routes.map(route=>func(route.geometry.coordinates))
+    
+    }catch (error){
+        console.error(error)
+    }finally{
+        console.log('finally')
+    }    
 }
- 
-
+        
 
 
